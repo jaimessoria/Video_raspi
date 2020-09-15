91c9dd9e92232d336a7bb778dc12243b5569030a
jaimessoria <l.jaimessoria@gmail.com>
2020-09-15 11:39:24 +0200

    tested

--- temp.py
+++ temp.py
@@ -1,7 +1,8 @@
 import os
 import time
-#from gpiozero import CPUTemperature
-#from time import sleep, strftime, time
+from gpiozero import CPUTemperature
+from time import sleep, strftime, time
+#from datetime import datetime
 
 NAS = '192.168.0.135:/Users/Leo/Documents/Raspi'
 folder = '/mnt/nfs'
@@ -14,18 +15,19 @@ for i in range(2):
     command = 'sudo mount -t nfs '+ NAS +' '+folder
     os.system(command)    
     #Capture Video
-    file_name= folder + time.strftime("/1080p30_b12-%Y%m%d-%H%M%S")
-    command = 'raspivid -t'+ str(tcap*100) +'-w 1436 -h 1080 -fps 30 -b 12000000 -o '+file_name +'.h264'
+    file_name= folder + strftime("/1080p30_b12-%Y%m%d-%H%M%S")
+    command = 'raspivid -t '+ str(t_cap*100) +' -w 1436 -h 1080 -fps 30 -b 12000000 -o '+file_name +'.h264' 
     os.system(command)
     sleep(t_cap)
     #convert video to mp4
     command= 'MP4Box -fps 30 -add '+file_name+ '.h264 '+ file_name +'.mp4'
     os.system(command)
+    sleep(t_cap)    
     # log temperature
-    log = open(folder+"cpu_temp.csv", "a")
+    log = open(folder+"/cpu_temp.csv", "a")
     log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),cpu.temperature))
     log.close()
-    #sleep(1)
+    sleep(1)
 #unmount NAS
 command = 'sudo umount /mnt/nfs'
 os.system(command)   
